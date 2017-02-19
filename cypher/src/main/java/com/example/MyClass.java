1ed16a523878b4341d3151623b08cf94a693a852
georgeapostol <LostSouls@Georges-iMac.local>
2017-02-19 14:18:37 -0600

    Initial commit

---
+++ cypher/src/main/java/com/example/MyClass.java
@@ -0,0 +1,55 @@
+package com.example;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        String S = "hi mom!";
+        System.out.println(S);
+
+        if (S.contains("h")) {
+            char h = '\u0062';
+        System.out.print(h);
+        }
+
+            if (S.contains("i")) {
+                char i = '\u0079';
+                System.out.print(i);
+            }
+
+                if (S.contains("m")) {
+            char m = '\u0064';
+            System.out.print(m);
+
+
+                if (S.contains("o")) {
+                        char o = '\u0075';
+                    System.out.print(o);
+                }
+                }
+
+                if (S.contains("m")) {
+            char m = '\u0064';
+            System.out.print(m);
+
+        }
+               if (S.contains("!")){
+                    char l = '\u0065';
+                    System.out.print(l);
+                }
+            }
+
+        }
+
+
+
+
+//        String s = "Hello, there.";
+//        System.out.println(s);
+//        s.toCharArray();
+//        char[] charArray = s.toCharArray();
+//        int[] intArray = new int[charArray.length];
+//        for(int i = 0; i < charArray.length; i++){
+//            intArray[i] = (int) charArray[i];
+//            intArray[i]+=3;
+//        }
+//        System.out.println(intArray);
